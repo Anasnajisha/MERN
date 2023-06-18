13e83ad5661ffb6b85a19d5a72b95c0a72a20d08
NPK13 <nishalbabupk@gmail.com>
2023-06-18 12:44:47 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,37 @@ const express = require('express');
 const app = express();
 const PORT = 4578
 const mongoose=require('mongoose');
+const cors = require('cors'); // to remore cor issue
+app.use(cors())  // cor policy activation
+app.use(express.json()); // to render json req from frontend
+app.use(express.urlencoded({extended:true}));
 
 
 mongoose.connect("mongodb+srv://najisha:4oLGwjTGmXn9O08T@cluster0.jssp599.mongodb.net/")
 .then(()=>{console.log("MongoDB connect successfully")})
-.catch((error)=>{console.log(error)});
+.catch((err) => { console.log("Error connecting to MongoDB " + err) });//*fail message
 
-app.get('/',(req,res)=>{
-    res.send('server is running')
+// CRUD operation 
+// C-Create - POST 
+//R-READ-GET
+//U-Update -PUT
+//D-Delete - DELETE
 
+app.post('/addData', (req, res) => {
+    try {
+
+        let item = req.body
+        console.log(item)
+
+
+
+    } catch (error) {
+        res.send(error);
+    }
 })
 
-app.get('/about',(req,res)=>{
-    res.send('  About')
 
-})
-
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
-})
+});
 
