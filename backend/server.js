21efe6d85d765ed1e2a8e77cf5cd1d1d43452e91
NPK13 <nishalbabupk@gmail.com>
2023-06-18 16:28:57 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors'); // to remore cor issue
 app.use(cors())  // cor policy activation
 app.use(express.json()); // to render json req from frontend
 app.use(express.urlencoded({extended:true}));
-mongoose.connect("mongodb+srv://najishathaikulathil:f47S3aEWZL0UyyuJ@cluster0.600aoqs.mongodb.net/MES")
+mongoose.connect("mongodb+srv://najishathaikulathil:f47S3aEWZL0UyyuJ@cluster0.600aoqs.mongodb.net/")
 .then(()=>{console.log("MongoDB connect successfully")})
 .catch((err) => { console.log("Error connecting to MongoDB " + err) });//*fail message
 
@@ -38,6 +38,60 @@ app.post('/addData',async (req, res) => {
 })
 
 
+
+//READ
+
+app.get('/getData', async (req, res) => {
+    try {
+        console.log("reading data")
+        const data = await PRODUCT.find({}) //code that find all data from backend
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+        res.send(error)
+    }
+})
+
+
+// For deleteing and updating we are using "ids" of entry and then do the procedd
+
+//UPDATE
+
+
+app.put('/updateData/:id', async (req, res) => {
+    try {
+
+        let id = req.params.id
+        let updateData = { $set: req.body }
+
+        const updated = await PRODUCT.findByIdAndUpdate(id, updateData)
+
+        res.json(updated)
+
+    }  catch (error) {
+        console.log(error)
+        res.send('error')
+    }
+
+})
+
+
+//DELETE
+router.delete('/:id', async (req, res) => {
+    try {
+        let id = req.params.id
+        console.log('id check', id)
+
+        const updated = await PRODUCT.findByIdAndDelete(id)
+
+        res.send("deleted successfully")
+
+    } catch (error) {
+        console.log(error)
+        res.send('error')
+    }
+})
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
+
